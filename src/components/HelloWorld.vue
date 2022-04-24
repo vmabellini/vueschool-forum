83e6a5567912cec73bb9144c244e45f710d63e1f
Vinicius Mabellini <vpmabellini@gmail.com>
2022-04-24 20:11:47 -0300

    adding css in assets and ref to it in App.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -31,6 +31,7 @@
 </template>
 
 <script>
+import sourceData from '@/data.json'
 export default {
   name: 'HelloWorld',
   props: {
---
+++ src/components/PageHome.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="col-large push-top" v-for='thread in threads' :key="thread.id">
+    <h1>{{thread.title}}</h1>
+
+    <div class="post-list">
+      <div class="post" v-for="postId in thread.posts" :key="postId">
+        <div class="user-info">
+          <a href="#" class="user-name">{{getUserById(getPostById(postId).userId).name}}</a>
+
+          <a href="#">
+              <img class="avatar-large" :src="getUserById(getPostById(postId).userId).avatar" alt="">
+          </a>
+
+          <p class="desktop-only text-small">107 posts</p>
+
+        </div>
+
+         <div class="post-content">
+            <div>
+              <p>
+                {{getPostById(postId).text}}
+              </p>
+            </div>
+        </div>
+
+        <div class="post-date text-faded">
+            {{getPostById(postId).publishedAt}}
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data.json'
+export default {
+  data () {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  },
+  methods: {
+    getPostById (postId) {
+      return this.posts.find(el => el.id === postId)
+    },
+    getUserById (userId) {
+      return this.users.find(el => el.id === userId)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
